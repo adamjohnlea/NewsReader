9612d1b8b4f119d235caaeda28e5dcea25d00a77
adamjohnlea <imnogeekdptcom@me.com>
2021-03-11 12:41:58 -0700

    Initial Commit

---
+++ NewsReader/NewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderApp.swift
+//  NewsReader
+//
+//  Created by Adam Lea on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
